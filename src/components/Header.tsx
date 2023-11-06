662305e0c88fc408de35bf5daab2b0534b877d50
dukartbr <dukartbrady@gmail.com>
2023-11-06 10:03:43 -0600

    v1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,55 +1,78 @@
 import {
-  Box,
-  Button,
-  Flex,
-  Spacer,
-  Text,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
+	Box,
+	Button,
+	Flex,
+	Icon,
+	Spacer,
+	Text,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalCloseButton,
+	useDisclosure,
+	Link,
 } from "@chakra-ui/react";
-import { FaLinkedin } from "react-icons/fa6";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <>
-      <Flex w='100%' bg='blue.300' py={2} px={8}>
-        <Box lineHeight='1.25'>
-          <Text fontSize='xl' fontWeight='bold'>
-            Brady Dukart
-          </Text>
-          <Text fontSize='md' fontStyle='italic'>
-            React Software Engineer
-          </Text>
-        </Box>
-        <Spacer />
-        <Button onClick={onOpen}>Connect</Button>
-      </Flex>
-      <ContactModal isOpen={isOpen} onClose={onClose} />
-    </>
-  );
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		<>
+			<Flex w="100%" bg="blue.300" py={2} px={8}>
+				<Box lineHeight="1.25">
+					<Text fontSize="xl" fontWeight="bold">
+						Brady Dukart
+					</Text>
+					<Text fontSize="md" fontStyle="italic">
+						React Software Engineer II
+					</Text>
+				</Box>
+				<Spacer />
+				<Button onClick={onOpen} colorScheme="yellow">
+					Connect
+				</Button>
+			</Flex>
+			<ContactModal isOpen={isOpen} onClose={onClose} />
+		</>
+	);
 }
 
 function ContactModal({
-  isOpen,
-  onClose,
+	isOpen,
+	onClose,
 }: {
-  isOpen: boolean;
-  onClose: () => void;
+	isOpen: boolean;
+	onClose: () => void;
 }) {
-  return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Contact</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>Howdy!</ModalBody>
-      </ModalContent>
-    </Modal>
-  );
+	return (
+		<Modal isOpen={isOpen} onClose={onClose}>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Let's Work Together!</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<Text>
+						You can reach me or check out my work here. I look forward to
+						working with you!
+					</Text>
+
+					<Flex py={8} width="100%" px={12}>
+						<Link href="https://www.linkedin.com/in/bradydukart/">
+							<Icon as={FaLinkedin} fontSize="50px" />
+						</Link>
+						<Spacer />
+						<Link href="https://github.com/">
+							<Icon as={FaGithub} fontSize="50px" />
+						</Link>
+						<Spacer />
+						<Link href="mailto: dukartbrady@gmail.com">
+							<Icon as={FaEnvelope} fontSize="50px" />
+						</Link>
+					</Flex>
+				</ModalBody>
+			</ModalContent>
+		</Modal>
+	);
 }
