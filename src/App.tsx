aad61b390500f1f42316799a1372c2580472337f
Brady Dukart <dukartbrady@gmail.com>
2024-04-28 15:17:13 -0500

    updates overflow

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import { Portfolio } from "./components/Portfolio";
 import { Tech } from "./components/Tech";
 
 function App() {
-	return (
-		<Box position="fixed" bgColor="gray.700" h="100vh" overflow="scroll">
-			<Header />
-			<Box w="100vw" pb={8}>
-				<Container maxW="container.lg">
-					<Bio />
-					<Portfolio />
-					<Tech />
-				</Container>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box position="fixed" bgColor="gray.700" h="100vh" overflowX="hidden">
+      <Header />
+      <Box w="100vw" pb={8}>
+        <Container maxW="container.lg">
+          <Bio />
+          <Portfolio />
+          <Tech />
+        </Container>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
